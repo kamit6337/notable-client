eaaabeb0214f07d3ed32059a7bd7707c0d3c8b2a
Kamit6337 <kamit6337@gmail.com>
2024-02-22 18:36:16 +0530

    Update

--- src/utils/environment.js
+++ src/utils/environment.js
@@ -1,6 +1,4 @@
 const environment = {
-  CRYPTO_SECRET_KEY: import.meta.env.VITE_APP_CRYPTO_SECRET_KEY,
-  CRYPTO_SECRET_VALUE: import.meta.env.VITE_APP_CRYPTO_SECRET_VALUE,
   SERVER_URL: import.meta.env.VITE_APP_SERVER_URL,
   NODE_ENV: import.meta.env.NODE_ENV,
 };
