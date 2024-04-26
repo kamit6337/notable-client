90ff38b325a241580651f30f4ae20f59e52fd482
Kamit6337 <kamit6337@gmail.com>
2024-04-26 16:46:09 +0530

    update all tags

--- src/pages/tags/AllTags.jsx
+++ src/pages/tags/AllTags.jsx
@@ -38,93 +38,95 @@ const AllTags = ({ reset }) => {
 
   return (
     <>
- <Helmet>
+      <Helmet>
         <title>Tags</title>
         <meta name="discription" content="A Note making Web Apps" />
       </Helmet>
 
-    <section className="px-5 tablet:px-2 w-full pb-5 h-full">
-      <main className="h-40 flex flex-col justify-center gap-6">
-        {/* MARK: PART-1 OF HEADER */}
-        <div>
-          <div className="flex justify-between items-center ">
-            <p className="text-xl font-semibold tracking-wide">Tags</p>
-            <div className="relative">
-              <p
-                className="p-2 cursor-pointer"
-                onClick={() => setShowOption(true)}
+      <section className="px-5 tablet:px-2 w-full pb-5 h-full">
+        <main className="h-40 flex flex-col justify-center gap-6">
+          {/* MARK: PART-1 OF HEADER */}
+          <div>
+            <div className="flex justify-between items-center ">
+              <p className="text-xl font-semibold tracking-wide">Tags</p>
+              <div
+                className="relative"
+                onMouseLeave={() => setShowOption(false)}
               >
-                <Icons.options />
-              </p>
-              {showOption && (
-                <div
-                  className="absolute z-10 top-full right-0 border rounded-md text-sm bg-slate-800 text-white whitespace-nowrap"
-                  onMouseLeave={() => setShowOption(false)}
+                <p
+                  className="p-2 px-4 pl-10 cursor-pointer"
+                  onClick={() => setShowOption(true)}
                 >
-                  <p
-                    className="py-3 px-5 cursor-pointer"
-                    onClick={() => dispatch(toggleCreateNewTag({ bool: true }))}
-                  >
-                    Create New Tag
-                  </p>
-                </div>
-              )}
+                  <Icons.options />
+                </p>
+                {showOption && (
+                  <div className="absolute z-10 top-full right-0 border rounded-md text-sm bg-slate-800 text-white whitespace-nowrap">
+                    <p
+                      className="py-3 px-5 cursor-pointer"
+                      onClick={() =>
+                        dispatch(toggleCreateNewTag({ bool: true }))
+                      }
+                    >
+                      Create New Tag
+                    </p>
+                  </div>
+                )}
+              </div>
             </div>
+            <p className="w-full h-[2px] mt-2 bg-black" />
           </div>
-          <p className="w-full h-[2px] mt-2 bg-black" />
-        </div>
 
-        {/* MARK: PART-2 OF HEADER */}
-        <div className="border rounded-md w-full">
-          <input
-            {...register("search")}
-            type="text"
-            onChange={handleSearch}
-            spellCheck="false"
-            autoComplete="off"
-            placeholder="Find Tag"
-            className="p-2 rounded-md w-full text-black bg-inherit border border-my_single_note_title"
-          />
-        </div>
-      </main>
+          {/* MARK: PART-2 OF HEADER */}
+          <div className="border rounded-md w-full">
+            <input
+              {...register("search")}
+              type="text"
+              onChange={handleSearch}
+              spellCheck="false"
+              autoComplete="off"
+              placeholder="Find Tag"
+              className="p-2 rounded-md w-full text-black bg-inherit border border-my_single_note_title"
+            />
+          </div>
+        </main>
 
-      {/* MARK: TAG LIST VERTICAL SCROLLABLE */}
-      <div
-        className={`overflow-y-auto  flex flex-col gap-10  `}
-        style={{ maxHeight: "calc(100% - 160px)" }}
-      >
-        {tags.length > 0 ? (
-          <>
-            {alphabet.map((letter, i) => {
-              if (!showSearchedTag) {
-                return (
-                  <ShowAlphabeticalTags
-                    letter={letter}
-                    reset={reset}
-                    tags={tags}
-                    key={i}
-                  />
-                );
-              } else if (searchTagList.length > 0) {
-                return (
-                  <ShowAlphabeticalTags
-                    letter={letter}
-                    reset={reset}
-                    tags={searchTagList}
-                    key={i}
-                  />
-                );
-              } else {
-                return;
-              }
-            })}
-            <div className="w-full basis-20 grow-0 shrink-0" />
-          </>
-        ) : (
-          <div>No tag available</div>
-        )}
-      </div>
-    </section>
+        {/* MARK: TAG LIST VERTICAL SCROLLABLE */}
+        <div
+          className={`overflow-y-auto  flex flex-col gap-10  `}
+          style={{ maxHeight: "calc(100% - 160px)" }}
+        >
+          {tags.length > 0 ? (
+            <>
+              {alphabet.map((letter, i) => {
+                if (!showSearchedTag) {
+                  return (
+                    <ShowAlphabeticalTags
+                      letter={letter}
+                      reset={reset}
+                      tags={tags}
+                      key={i}
+                    />
+                  );
+                } else if (searchTagList.length > 0) {
+                  return (
+                    <ShowAlphabeticalTags
+                      letter={letter}
+                      reset={reset}
+                      tags={searchTagList}
+                      key={i}
+                    />
+                  );
+                } else {
+                  return;
+                }
+              })}
+              <div className="w-full basis-20 grow-0 shrink-0" />
+            </>
+          ) : (
+            <div>No tag available</div>
+          )}
+        </div>
+      </section>
     </>
   );
 };
